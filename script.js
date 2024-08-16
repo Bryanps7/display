e3ea5af37f83591fa054479ccb1c18f8fc9532bf
Bryan Prinz <146386080+Bryanps7@users.noreply.github.com>
2024-08-16 00:01:32 -0300

    Versão 2.0.0

--- script.js
+++ script.js
@@ -6,15 +6,16 @@ function gerar() {
     let text = document.getElementById('text').value
     let corBack = document.getElementById('corBack').value
     let corText = document.getElementById('corText').value
+    let speed = Number(document.getElementById('speed').value)
 
     if(corBack == 'vini') {
         console.log('estou aqui')
         container.innerHTML = `
-            <marquee style="background: linear-gradient(to right, red, orange, yellow, green, blue, violet, purple); color: ${corText};">${text}</marquee>
+            <marquee scrollamount='${speed}' style="background: linear-gradient(to right, red, orange, yellow, green, blue, violet, purple); color: ${corText};">${text}</marquee>
             `
     }else {
         container.innerHTML = `
-            <marquee style="background-color: ${corBack}; color: ${corText};">${text}</marquee>
+            <marquee scrollamount='${speed}' style="background-color: ${corBack}; color: ${corText};">${text}</marquee>
         `
     }
 }
